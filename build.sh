c48532a7afae3273c73a1141ccba2f57023c43d8
glindor <greglindor1@gmail.com>
2015-04-22 09:28:19 -0400

    Update config

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+#set up the directory to install cuckoo
 USER=`whoami`
 sudo mkdir /data
 sudo chown -R $USER:$USER /data
 
+#update system
 sudo apt-get update -y && sudo apt-get upgrade -y
 
+#install dependencies
 sudo apt-get install -y vim screen unzip python python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-chardet tcpdump libcap2-bin python-dev build-essential subversion pcregrep libpcre++-dev python-pip ssdeep libfuzzy-dev git automake libtool autoconf libapr1 libapr1-dev libnspr4-dev libnss3-dev libwww-Perl libcrypt-ssleay-perl python-dev python-scapy python-yaml bison libpcre3-dev bison flex libdumbnet-dev autotools-dev libnet1-dev libpcap-dev libyaml-dev libnetfilter-queue-dev libprelude-dev zlib1g-dev libz-dev libcap-ng-dev libmagic-dev python-mysqldb cmake libjansson-dev libcdio-utils mongodb-server python-simplejson p7zip-full libzzip-dev python-geoip python-chardet python-m2crypto python-dnspython 
 
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-sudo pip install bottle django pycrypto clamd distorm3 pygal django-ratelimit 
+sudo pip install bottle django pycrypto distorm3 pygal django-ratelimit 
 
 
 #Set up PEFILE dependency for Yara
-wget https://pefile.googlecode.com/files/pefile-1.2.10-139.tar.gz
+wget http://pefile.googlecode.com/files/pefile-1.2.10-139.tar.gz
 tar -xzvf pefile-1.2.10-139.tar.gz
 cd pefile-1.2.10-139
 python setup.py build
@@ -24,6 +27,7 @@ cd ..
 git clone https://github.com/plusvic/yara
 cd yara
 ./bootstrap.sh
+./configure --enable-cuckoo --enable-magic
 chmod +x build.sh
 ./build.sh
 sudo make install
@@ -50,6 +54,8 @@ python setup.py build
 sudo python setup.py install
 cd ..
 
+
+#install cuckoo sandbox
 git clone https://github.com/brad-accuvant/cuckoo-modified.git cuckoo
 cd cuckoo/utils
 ./community.py -a -f
@@ -78,5 +84,14 @@ echo $file
 wget http://download.virtualbox.org/virtualbox/$var1/$file -O /tmp/$file
 #sudo VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
 sudo VBoxManage extpack install /tmp/$file --replace
+
+#run python script to create the VirtualBox Guest
+python buildVMXP.py cuckoo1
+
+#set up IP and forwarding
+sudo iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A POSTROUTING -t nat -j MASQUERADE
+sudo sysctl -w net.ipv4.ip_forward=1
 
 
